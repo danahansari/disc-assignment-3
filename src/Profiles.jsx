1280efb7e1feddc72a5923e510384a57c8e577a5
Danah Ansari <danahansari@dhcp-10-105-189-79.wireless.northwestern.private>
2025-10-20 15:39:47 -0500

    Tried to fix .content by removing min-height

--- src/Profiles.jsx
+++ src/Profiles.jsx
@@ -7,6 +7,21 @@ function Profiles() {
       <div className="grid-item">Profile 4</div>
       <div className="grid-item">Profile 5</div>
       <div className="grid-item">Profile 6</div>
+      <div className="grid-item">Profile 7</div>
+      <div className="grid-item">Profile 8</div>
+      <div className="grid-item">Profile 9</div>
+      <div className="grid-item">Profile 10</div>
+      <div className="grid-item">Profile 11</div>
+      <div className="grid-item">Profile 12</div>
+      <div className="grid-item">Profile 13</div>
+      <div className="grid-item">Profile 14</div>
+      <div className="grid-item">Profile 15</div>
+      <div className="grid-item">Profile 16</div>
+      <div className="grid-item">Profile 17</div>
+      <div className="grid-item">Profile 18</div>
+      <div className="grid-item">Profile 19</div>
+      <div className="grid-item">Profile 20</div>
+      <div className="grid-item">Profile 21</div>
     </>
   );
 }
